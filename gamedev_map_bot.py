a686f1bec5dc2a037deb49ac6af83d9c3859be2c
L4ZZA <pietro.lazz@gmail.com>
2020-02-13 17:49:04 +0000

    added logging method

--- gamedev_map_bot.py
+++ gamedev_map_bot.py
@@ -1,28 +1,32 @@
 import sys
 import csv
 import unicodedata
+
 from selenium import webdriver
 from time import sleep
+from datetime import datetime
 
 class bot():
+    def logMessage(self, message, toConsole=True):
+        timeNow = datetime.now().strftime("%d.%m.%Y %H:%M:%S:%f")
+        logLine = timeNow + " | " + message
+        if toConsole:
+            print logLine
+        self.log.append(logLine)
+
     def __init__(self):
         self.driver = webdriver.Chrome()
         self.driver.get("https://www.gamedevmap.com/")
 
-        global header
-        global companyRows
-        companyRows = []
-        header = []
+        self.companyRows = []
+        self.header = []
+        self.log = []
+        self.logMessage("Bot initialized")
         companySelect = self.driver.find_element_by_id('typeDropdown').find_elements_by_tag_name("option")
         companyTypes = [elem for elem in companySelect if elem.get_attribute("value") != ""]
-        self.typeNames = [t.get_attribute("value") for t in companyTypes]
-        print "Company types:"
-        for element in self.typeNames:
-            print "- " + element
-        # print "Countries:"
-        # for element in self.countries:
-        #     print "- " + element.get_attribute("value")
         
+        self.typeNames = [t.get_attribute("value") for t in companyTypes]
+        self.logMessage("Company types: " + ','.join(self.typeNames))
     
     def selectType(self, name):
         companySelect = self.driver.find_element_by_id('typeDropdown').find_elements_by_tag_name("option")
@@ -35,7 +39,7 @@ class bot():
         for country in countries:
             tmpName = country.get_attribute("value")
             if name == tmpName:
-                print "Selected country - " + tmpName
+                self.logMessage("Selected country - " + tmpName)
                 country.click()
             
     def parseHeader(self, headerRow):
@@ -51,19 +55,18 @@ class bot():
     def parseTable(self):
         table = self.driver.find_element_by_xpath('/html/body/table/tbody/tr/td/div[3]/table[3]')
         rows = table.find_elements_by_tag_name('tr')
-        global header
-        header = self.parseHeader(rows[3])
-        header = ','.join(header)
-        print "-------" + header + "--------"
+        
+        self.header = self.parseHeader(rows[3])
+        self.header = ','.join(self.header)
+        self.logMessage("-------" + self.header + "--------")
         i = 1
         for row in rows[4:-1]: # from 4th row to second last (included)
-            global companyRows
             companyRow = self.parseCompany(row)
             companyRow = ','.join(companyRow)
-            companyRows.append(companyRow)
-            print i, " - ", companyRow
+            self.companyRows.append(companyRow)
+            self.logMessage(str(i) + " - " + companyRow)
             i += 1
-        print "-----------------------------"
+        self.logMessage("-----------------------------")
 
     def getNext(self):
         try:
@@ -78,11 +81,11 @@ class bot():
         for t in reversed(self.typeNames):
             self.selectType(t) # select dropdown item
             sleep(0.1) # wait for page to load
-            print "- " + t + "- parsing..."
+            self.logMessage("- " + t + "- parsing...")
             pageN = 1
             
             while True:
-                print "- page ", pageN
+                self.logMessage("- page " + str(pageN))
                 self.parseTable()
                 # return # uncomment to parse first page
                 nextPageBtn = self.getNext()
@@ -91,21 +94,28 @@ class bot():
                     pageN += 1
                     sleep(0.1)
                 else:
-                    print "- " + t + "- completed."
-                    return # uncomment to parse first company type only
+                    self.logMessage(str("- " + t + "- completed."))
+                    # return # uncomment to parse first company type only
                     break
             
     def dump(self):
         with open('gamedevmap.csv','wb') as file:
-            global header
-            file.write(header)
+            
+            file.write(self.header)
             file.write("\n")
-            global companyRows
-            for row in companyRows:
+            
+            for row in self.companyRows:
                 file.write(row)
+                file.write('\n')
+            
+    def dumpLog(self):
+        with open('log.txt','wb') as file:
+            for line in self.log:
+                file.write(line)
                 file.write('\n')
 
 b = bot()
 sleep(0.2)
 b.allCompByType()
 b.dump()
+b.dumpLog()
